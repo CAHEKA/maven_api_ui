1cb8658199869d5752894561353f49e5b086f646
Kamil <k.sabuev@datamart.ru>
2024-04-05 23:16:48 +0300

    Initial commit

---
+++ api-tests/src/test/java/ApiTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ApiTest {
+    @Test
+    public void apiTest() {
+        assertEquals(1, 1);
+    }
+    
+    @Test
+    @Tag("smoke")
+    public void apiTagTest() {
+        assertEquals(1, 1);
+    }
+}
